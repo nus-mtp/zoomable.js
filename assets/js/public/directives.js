60787339f6f4a29e627ab7e210b6a1de64f7e8c1
kphua <kathyphua92@gmail.com>
2016-01-27 18:52:25 +0800

    Remove redundant code from navbar directive, add variables needed into loginController.js

--- assets/js/public/directives.js
+++ assets/js/public/directives.js
@@ -4,11 +4,6 @@ angular.module('zoomableApp')
     return {
       restrict: 'A', //This means that it will be used as an attribute and NOT as an element.
       replace: true,
-      scope: {},
-      templateUrl: "../../../views/navbar.html",
-      controller: ['$scope', '$filter', function ($scope, $filter) {
-          // TODO: USER AUTHENTICATION TO SHOW OR HIDE NAVBAR
-          $scope.user = true;  // set to true for now
-      }]
+      templateUrl: "../../../views/navbar.html"
     }
   });
