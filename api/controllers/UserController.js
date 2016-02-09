f13f606a56217e9eaa05e42bd62331217d094ded
jiale <jiale@comp.nus.edu.sg>
2016-02-09 22:24:41 +0800

    Add logout method to UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -95,8 +95,23 @@ module.exports = {
    * `UserController.logout()`
    */
   logout: function (req, res) {
-    return res.json({
-      todo: 'logout() is not implemented yet!'
+    // Look up the user record from the database which is
+    // referenced by the id in the user session (req.session.me)
+    User.findOne(req.session.me, function foundUser(err, user) {
+      if (err) return res.negotiate(err);
+
+      // If session refers to a user who no longer exists, still allow logout.
+      if (!user) {
+        sails.log.verbose('Session refers to a user who no longer exists.');
+        return res.backToHomePage();
+      }
+
+      // Wipe out the session (log out)
+      req.session.me = null;
+
+      // Either send a 200 OK or redirect to the home page
+      return res.backToHomePage();
+
     });
   },
 
