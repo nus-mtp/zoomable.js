357f388e7612c9fd9caaed74b79ff7649697302b
kphua <kathyphua92@gmail.com>
2016-02-13 01:09:34 +0800

    Update edit page to show video fields correctly

--- assets/js/public/controllers/editController.js
+++ assets/js/public/controllers/editController.js
@@ -3,11 +3,14 @@ angular.module('zoomableApp').controller('editController', function($scope, $sta
 	// VARIABLES
 	$scope.defaultImagePath = 'images/bunny.png';
 	$scope.video_id = $stateParams.videoId;
+	$scope.originalVideoTitle = '';
 
 	/* Get video object by video id */
 	servicesAPI.getOne($scope.video_id)
 	.success(function(data) {
 		$scope.video = data;
+		// prevent page header from changing when title is being edited
+		$scope.originalVideoTitle = $scope.video.title;
 	})
 	.error(function(data) {
 		console.log('Error: ' + data);
