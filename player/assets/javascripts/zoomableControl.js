6a55a541404afc1cca8cbf6956a1dba7aecaaa34
Yui Wei <tyuiwei@gmail.com>
2016-02-23 13:48:16 +0800

    initial commit (skeleton)

---
+++ player/assets/javascripts/zoomableControl.js
@@ -0,0 +1,25 @@
+var Zoomable = function(player_list) {
+    init_players(player_list);
+    function init_players(player_list) {
+        
+    }
+    
+    this.time = ;
+    
+    this.play = ;
+    this.pause = ;
+    this.seek = ;
+    this.volume = ;
+    this.controls = ;
+    this.zoom = ;
+    this.pan = ;
+    
+    var Play = function() {};
+    var Pause = function() {};
+    var Seek = function() {};
+    var Volume = function() {};
+    var Controls = function() {};
+    var Zoom = function() {};
+    var Pan = function() {};
+    
+}
