352b1f0848cd5a61ad6d6dc6ced2f14d2c94716b
kphua <kathyphua92@gmail.com>
2016-03-08 18:46:07 +0800

    Add left menu bar and basic control

--- assets/js/public/controllers/loginController.js
+++ assets/js/public/controllers/loginController.js
@@ -1,4 +1,4 @@
-angular.module('zoomableApp').controller('loginController', function($scope, $state, servicesAPI){
+angular.module('zoomableApp').controller('loginController', function($scope, $state, servicesAPI, $mdSidenav){
   // VARIABLES
   $scope.username = '';
   $scope.password = '';
@@ -17,14 +17,71 @@ angular.module('zoomableApp').controller('loginController', function($scope, $st
     else {
       return false;
     }
-  }
+  };
+
+  /* Function to toggle left menu in dashboardController */
+  $scope.toggleLeftMenu = function() {
+    $mdSidenav('left').toggle();
+  };
+
+  /* Function to show the correct page on button selection */
+  $scope.showPage = function(event) {
+    // remove previous selected class
+    $('.selection-btn').removeClass('selected');
+
+    if (event.target.id === 'showVideoBtn') {
+      // add selected class
+      $('#showVideoBtn').addClass('selected');
+      // show video page - WIP
+    }
+    else if (event.target.id === 'showStatBtn') {
+      // add selected class
+      $('#showStatBtn').addClass('selected');
+      // show statistic page - WIP
+    }
+
+    // toggle the menu
+    $scope.toggleLeftMenu();
+  };
+
+  /* Function to watch toggling of left menu to set appropriate classes for styling */
+  $scope.$watch(function () {
+    return $mdSidenav('left').isOpen();
+  },
+  function (currValue, prevValue) {
+    if (currValue === false && prevValue === false) {
+      // don't set classes on initialization
+      return;
+    }
+    else {
+      // set classes
+      setClassesForLeftMenu();
+    }
+  });
 
   /* Function to call search in dashboardController */
   $scope.searchVideoList = function() {
     $scope.$emit('searchEmit', $scope.searchQuery);
+  };
+
+  function setClassesForLeftMenu() {
+    if ($('#menuBtn').hasClass('opened')) {
+      // remove background color on menu btn
+      $('#menuBtn').removeClass('opened');
+      // make body scrollable
+      $('body').removeClass('no-scroll');
+    }
+    else {
+      // add background color on menu bar
+      $('#menuBtn').addClass('opened');
+      // make body not scrollable
+      $('body').addClass('no-scroll');
+    }
   }
 
   // FUNCTIONS FOR LOGIN FORM
+
+  /* Function to sign in or create new account */
   $scope.submitForm = function() {
     if ($scope.isCreate) {
       var accountData = {
@@ -65,6 +122,7 @@ angular.module('zoomableApp').controller('loginController', function($scope, $st
     }
   };
 
+  /* Function to check for empty fields in form */
   $scope.hasEmptyFields = function() {
     if ($scope.isCreate) {
       // enable submit only if all fields are entered
@@ -79,6 +137,6 @@ angular.module('zoomableApp').controller('loginController', function($scope, $st
       }
     }
     return true;
-  }
+  };
 
 });
