a30c60e57956b73736581746a0e161a45a05edd2
kphua <kathyphua92@gmail.com>
2016-03-15 12:15:40 +0800

    Update default dates for statistic view

--- assets/js/public/controllers/statisticController.js
+++ assets/js/public/controllers/statisticController.js
@@ -1,10 +1,10 @@
 angular.module('zoomableApp').controller('statisticController', function($scope, moment){
   // VARIABLES
   $scope.criteria = 'DAY';  // default set to day for date criteria
-  $scope.startDate = moment().subtract(1, 'month').toDate();  // default start date is previous month
-  $scope.minDate = moment().subtract(1, 'month').toDate();  // default min date is previous month
-  $scope.endDate = new Date();  // default end date is current date
-  $scope.maxDate = new Date();  // default max date is current date
+  $scope.startDate = moment().subtract(1, 'months').toDate();  // default start date is previous month
+  $scope.minDate = moment().subtract(1, 'months').toDate();  // default min date is previous month
+  $scope.endDate = moment().subtract(1, 'days').toDate();  // default end date is current date - 1
+  $scope.maxDate = moment().subtract(1, 'days').toDate();  // default max date is current date - 1
 
   /* Function to set date criteria */
   $scope.updateCriteria = function(event) {
