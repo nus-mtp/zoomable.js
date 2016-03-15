529b244e264f79e8a923ea15eda628f20fa960f4
kphua <kathyphua92@gmail.com>
2016-03-15 09:23:57 +0800

    Add date selection for statistic view

--- assets/js/public/controllers/statisticController.js
+++ assets/js/public/controllers/statisticController.js
@@ -1,6 +1,10 @@
-angular.module('zoomableApp').controller('statisticController', function($scope){
+angular.module('zoomableApp').controller('statisticController', function($scope, moment){
   // VARIABLES
   $scope.criteria = 'DAY';  // default set to day for date criteria
+  $scope.startDate = moment().subtract(1, 'month').toDate();  // default start date is previous month
+  $scope.minDate = moment().subtract(1, 'month').toDate();  // default min date is previous month
+  $scope.endDate = new Date();  // default end date is current date
+  $scope.maxDate = new Date();  // default max date is current date
 
   /* Function to set date criteria */
   $scope.updateCriteria = function(event) {
