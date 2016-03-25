0be1a7e333b327dbda0ddaee6bc2fc4e0ea8ec75
kphua <kathyphua92@gmail.com>
2016-03-25 13:00:53 +0800

    Add new models for statistics

---
+++ api/models/ViewData.js
@@ -0,0 +1,30 @@
+/**
+* ViewData.js
+*
+* @description :: This model represents the data for all the video statistics.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	attributes: {
+		coordinates: {
+			type: 'array',
+			required: true
+		},
+
+		width: {
+			type: 'float',
+			required: true
+		},
+
+		videoTime: {
+			type: 'float',
+			required: true
+		},
+
+		// Add a reference to ViewSession
+    sessionObj: {
+      model: 'ViewSession'
+    }
+	}
+}
