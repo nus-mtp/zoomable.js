c44a8cad97cb9f759b2c98895750368c43443e54
Yui Wei <tyuiwei@gmail.com>
2016-03-29 01:30:38 +0800

    Draw video to minimap

--- assets/js/public/player/minimap.js
+++ assets/js/public/player/minimap.js
@@ -3,6 +3,7 @@ var Minimap = function(canvas, video, parent) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.video = video;
+    this.transforms = new Transforms(this);
     this.outline = new Outline(this);
 
     var Outline = function(minimap) {
@@ -14,5 +15,22 @@ var Minimap = function(canvas, video, parent) {
             minimap.ctx.rect(x,y,width,height);
             minimap.ctx.stroke();
         }
+    }
+
+    var Transforms = function(minimap) {
+		minimap.video.addEventListener('play', function() {
+			minimap.transforms.draw();
+		},false);
+
+		this.draw = function() {
+			if (!minimap.video.paused) { 	//if(v.paused || v.ended) return false;
+				//slave.frameCnt += 1;
+				//console.log(slave.frameCnt);
+				minimap.ctx.drawImage(minimap.video);    //,coords.x,coords.y,dims.width,dims.height);
+			}
+			//slave.ctx.drawImage(slave.video,coords.x,coords.y,dims.width,dims.height);
+			requestAnimationFrame(minimap.transforms.draw)
+			//setTimeout(slave.transforms.draw,20);
+		}
     }
 }
